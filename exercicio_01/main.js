626f3ad3fa72613bcfbfaff1a551a048c1f1b765
Felipe Godoy <felipedegodoy.dev@gmail.com>
2024-12-18 10:14:52 -0300

    exercicios

---
+++ exercicio_01/main.js
@@ -0,0 +1,9 @@
+// function maiorValor(x, y) {
+//     return x > y ? x : y;
+// }
+
+// console.log(maiorValor(6, 2));
+
+const max = (x, y) => x > y ? x : y;
+
+console.log(max(6, 2));
---
+++ exercicio_02/main.js
@@ -0,0 +1,11 @@
+// function ePaisagem(largura, altura) {
+//     return largura > altura;
+// }
+
+// console.log(ePaisagem(1, 2));
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
+
+console.log(ePaisagem(1080, 1920));
