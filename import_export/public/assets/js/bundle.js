cd005ace26438954bd59a149989179ac9701a41a
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-04-11 12:54:35 -0300

    import/export, babel, webpack e geradorCpf

---
+++ import_export/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Luiz","Miranda",30),console.log(10)})();
+//# sourceMappingURL=bundle.js.map
---
+++ import_export/src/index.js
@@ -0,0 +1,4 @@
+import { nome2, sobrenome as seila, idade, soma } from './modulo1';
+
+console.log(nome2, seila, idade);
+console.log(soma(5, 5));
