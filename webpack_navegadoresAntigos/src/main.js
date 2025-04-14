5c5dc34fcc0229eed0b64a3ea493108dd5e12862
Felipe Godoy <felipedegodoy.dev@gmail.com>
2025-04-14 15:56:51 -0300

    gerador de senhas

---
+++ webpack_navegadoresAntigos/src/main.js
@@ -0,0 +1,7 @@
+import 'core-js/stable'; // usado para rodar algumas funções mais atuais do js em navegadores mais antigos
+import 'regenerator-runtime/runtime'; // usado para rodar algumas funções mais atuais do js em navegadores mais antigos
+
+import executa from './modules/promises';
+import './assets/css/style.css';
+
+executa();
---
+++ webpack_navegadoresAntigos/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path'); // CommonJS
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{ // esse rules comporta um array e pra cada tipo de arquivo são definidas algumas regras, nesse caso usaremos apenas para js, mas se tivesse algo para um arquivo css por exemplo teríamos que criar outro objeto com regras para esse tipo de arquivo
+            exclude: /node_modules/, // excluindo a pasta node_modules para que o babel não fique traduzindo os arquivos dessa pasta, tem muitos arquivos nessa pasta
+            test: /\.js$/, // expressão para falar o tipo do arquivo que irá compor o meu arquivo bundle
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }, 
+        {
+            test: /\.css/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    devtool: 'source-map' // usado para mostrar onde foi o erro, o arquivo bundle comporta todos os arquivos 'js' da aplicação, então utilizando esse 'devtool', irá mostrar em que arquivo original foi o erro, se não utilizar isso ele vai dar erro na linha por exemplo 150 do bundle e você não saberá que linha é essa no seu arquivo original 
+};
